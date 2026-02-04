84d41290915fad700ea9179a54c484678f613168
h.lapointe <h.lapointe@opendrives.com>
2026-02-04 10:10:19 -0500

    Initialize manager commands

--- server/manager/cmd/lnetDebug.go
+++ server/manager/cmd/lnetDebug.go
@@ -1,10 +1,67 @@
-package main
+/*
+Copyright © 2026 GlimmerFS Project
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+package cmd
 
 import (
 	"encoding/binary"
 	"fmt"
 	"net"
+
+	"github.com/spf13/cobra"
 )
+
+// lnetDebugCmd represents the lnetDebug command
+var lnetDebugCmd = &cobra.Command{
+	Use:   "lnet-debug",
+	Short: "Debug an LNet TCP Connection from a Lustre Client",
+	Long: `This performs initial communication to help troubleshoot lnet implementation.
+
+	Only TCP is supported.
+	`,
+	Run: func(cmd *cobra.Command, args []string) {
+		listener, err := net.Listen("tcp", fmt.Sprintf(":%d", Port))
+		if err != nil {
+			panic(err)
+		}
+		defer listener.Close()
+		fmt.Printf("Glimmer listening on %d...\n", Port)
+
+		for {
+			conn, err := listener.Accept()
+			if err == nil {
+				go handleConnection(conn)
+			}
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(lnetDebugCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// lnetDebugCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// lnetDebugCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
 
 // Constants from lustre_idl.h and socklnd.h
 const (
@@ -27,22 +84,6 @@ type HelloMsg struct {
 	Incarnation  uint64 // Connection instance ID
 	Type         uint32 // Message type (HELLO vs NOOP)
 	IsBodyOneReg uint32 // Capability flag
-}
-
-func main() {
-	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", Port))
-	if err != nil {
-		panic(err)
-	}
-	defer listener.Close()
-	fmt.Printf("Glimmer listening on %d...\n", Port)
-
-	for {
-		conn, err := listener.Accept()
-		if err == nil {
-			go handleConnection(conn)
-		}
-	}
 }
 
 func handleConnection(conn net.Conn) {
